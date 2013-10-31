3f9254755d9a30cfb9a1c4148e2177301f67b73a
Glenn <glenn@fireworkswebsites.com.au>
2013-11-01 05:08:19 +1000

    put garbage gallery into a class, went way overboard with debug output etc, it's a bit less garbage now

--- php/grbggal.php
+++ php/grbggal.php
@@ -1,21 +1,169 @@
 <?php
-$thumb = '/dev/shm/imglist_' . time();
-$imgs = array_merge(glob('*png'),glob('*jp*g'), glob('*gif'));
-ob_start();
-if ((is_dir($thumb) || mkdir($thumb)) && (file_exists('sm') || symlink($thumb, 'sm'))) {
-	foreach ($imgs as $img) {
-		if (!file_exists('sm/' . $img)) {
-			exec('convert "' . $img . '" -resize 200x200 "sm/' . $img . '"');
-		}
-		echo $img . '<br /><img src="sm/' . $img . '"><hr />' . "\n"; 
+class GarbageGallery {
+	public $workpath;
+	public $temppath;
+	public $thumbpath;
+	public $image_globs;
+	public $symlink_name;
+	public $thumb_size;
+	public $debug = FALSE;
+
+	public function __construct(
+								$temp_path = '/dev/shm', 
+								$image_globs = array('*png','*jp*g','*gif'),
+								$symlink_name = 'sm',
+								$thumb_size = 200
+								) {
+		$this->thumbpath = '';
+		$this->temppath = is_dir($temp_path) ? $temp_path : FALSE;
+		$this->image_globs = $image_globs;
+		$this->symlink_name = $symlink_name;
+		$this->thumb_size = $thumb_size;
+
+		$this->chdir();
+
+		// automatically output debugging garbage if debug is in the URL or querystring
+		if (strpos($_SERVER['QUERY_STRING'], 'debug') || strpos($_SERVER['REQUEST_URI'], 'debug'))
+			$this->debug();
 	}
-} else {
-	foreach ($imgs as $img) { 
-		echo $img . '<br /><img src="' . $img . '" height="150"><hr />' . "\n"; 
+
+	public function debug() {
+		$this->debug = TRUE;
+	}
+	private function log($method, $msg) {
+		if ($this->debug) {
+			echo __CLASS__ . ' ' . $method . ":\n<br />";
+			echo $msg . "\n<br />";
+		}
+	}
+
+	private function chdir() {
+		// find the path the user wants to view, this file probly isn't in that dir
+		$this->workpath = realpath($_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['REQUEST_URI']));
+		// change into it, things will be easier
+		chdir($this->workpath);
+
+		$this->log(__METHOD__, 'Changing directory to ' . $this->workpath);
+	}
+
+	public function getThumbPath() {
+		preg_match_all('/[A-Za-z0-9]+/', $this->workpath, $matches);
+		foreach ($matches as $m=>$match) 
+			$this->thumbpath .= implode('_', $matches[0]);
+		
+		$this->log(__METHOD__, 'temppath=' . $this->temppath);
+		$temp = explode(DIRECTORY_SEPARATOR, $this->temppath);
+		print_r($temp);
+		$temp[] = $this->thumbpath;
+		$this->thumbpath=implode(DIRECTORY_SEPARATOR, $temp);
+		
+		$this->log(__METHOD__, 'thumbpath=' . $this->thumbpath);
+		return $this->thumbpath;
+	}
+
+	/**
+	 * returns true if thumbnail dir already exists or has been created,
+	 * otherwise false
+	 */
+	public function createThumbdir($thumbpath) {
+		if (!is_dir($thumbpath)) {
+			$this->log(__METHOD__, 'creating thumb dir ' . $thumbpath);
+			mkdir($thumbpath);
+			
+			if (!is_dir($thumbpath)) {
+				$this->log(__METHOD__, 'Failed to create thumb dir!');
+				return FALSE;
+			}
+		}
+		$this->log(__METHOD__, 'thumbnail dir exists');
+		return TRUE;
+	}
+
+	/**
+	 k* returns true if symlink already exists or has been created
+	 * otherwise false
+	 */
+	public function createSymLink($linkname) {
+		if (!file_exists($linkname)) {
+			$this->log(__METHOD__, 'creating symlink ' . $linkname);
+			symlink($this->thumbpath, $linkname);
+
+			if (!file_exists($linkname)) {
+				$this->log(__METHOD__, 'Failed to create symlink!');
+				return FALSE;
+			}
+		}
+		$this->log(__METHOD__, 'symlink exists');
+		return TRUE;
+	}
+
+	/** 
+	 * Get a list of images in the current path that match our list of
+	 * filename globs 
+	 * Returns array of files that match
+	 */
+	public function listImages() {
+		$this->log(__METHOD__, 'globs: ' . implode(', ', $this->image_globs));
+		$images = array();
+		foreach ($this->image_globs as $glob) {
+			$images = array_merge($images, glob($glob));
+		}
+		$this->log(__METHOD__, 'Found ' . sizeof($images) . ' images');
+
+		return $images; // array_merge(glob('*png'),glob('*jp*g'), glob('*gif'));
+	}
+
+	public function thumbExists($img) {
+		return file_exists($this->symlink_name . DIRECTORY_SEPARATOR . $img);
+	}
+
+	public function createThumb($img) {
+		if (!$this->thumbExists($img)) {
+			$sz = "{$this->thumb_size}x{$this->thumb_size}";
+
+			// just kinda hoping imagemagick convert is installed and on the path...
+			// should use PHP imagemagick/GD libs
+			$exec_str = "convert \"$img\" -resize $sz \"{$this->symlink_name}" . DIRECTORY_SEPARATOR . "$img\"";
+			$this->log(__METHOD__, 'Creating thumbnail: <pre>' . $exec_str . '</pre>');
+			exec($exec_str);
+			if ($this->thumbExists($img))
+				$this->log(__METHOD__, 'Success');
+			else
+				$this->log(__METHOD__, 'Failed');
+		}
+		return $this->thumbExists($img);
+	}
+
+	/**
+	 * render the list of images in HTML, with links to their thumbnails if they were 
+	 * created. 
+	 * Param: $buffer boolean If TRUE, buffer and return the output from this function
+	 */
+	public function render($buffer = FALSE) {
+		if ($buffer) ob_start();
+		
+		if ($this->createThumbdir($this->getThumbPath()) && $this->createSymLink($this->symlink_name)) {
+			foreach ($this->listImages() as $img) {
+				$this->createThumb($img);
+				echo $img . '<br /><img src="' . $this->symlink_name . '/' . $img . '"><hr />' . "\n"; 
+			}
+		} else {
+			$this->log(__METHOD__, 'Failed to create thumbdir or symlink, going solo');
+
+			foreach ($this->listImages() as $img) {
+				echo $img . '<br /><img src="' . $img . '" height="150"><hr />' . "\n"; 
+			}
+		}
+		
+		if ($buffer) 
+			return ob_get_clean();
+		else
+			return TRUE;
 	}
 }
-$imgs = ob_get_clean();
 
+$g = new GarbageGallery();
+$buf = $g->render(TRUE);
 ?>
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><style>
 	* {
@@ -45,7 +193,7 @@ $imgs = ob_get_clean();
 					}
 				}
 				var img = document.createElement('img');
-				img.src = this.src.replace('/sm', '');
+				img.src = this.src.replace('<?= $g->symlink_name ?>', '');
 				img.setAttribute('class', 'fs');
 				img.setAttribute('style', 'position: fixed; top: 10px; right: 10px; z-index: 100; border: solid 2px #000; ');
 				document.body.appendChild(img);
@@ -55,4 +203,4 @@ $imgs = ob_get_clean();
 	}
 </script>
 
-</head><body><?= $imgs ?></body></html>
+</head><body><?= $buf ?></body></html>
