9b68fea57a82b1175056ec6f7205e2dd6e111916
Glenn <glenn@fireworkswebsites.com.au>
2013-11-01 00:48:39 +1000

    simplest possible image gallery from a list of files in a directory. creates thumbnails in /dev/shm if possible

---
+++ php/simple_image_list.php
@@ -0,0 +1,52 @@
+<?php
+$thumb = '/dev/shm/imglist_' . time();
+$imgs = array_merge(glob('*png'),glob('*jp.g'), glob('*gif'));
+ob_start();
+if ((is_dir($thumb) || mkdir($thumb)) && (file_exists('sm') || symlink($thumb, 'sm'))) {
+	foreach ($imgs as $img) {
+		if (!file_exists('sm/' . $img)) {
+			exec('convert "' . $img . '" -resize 200x200 "sm/' . $img . '"');
+		}
+		echo $img . '<br /><img src="sm/' . $img . '"><hr />' . "\n"; 
+	}
+} else {
+	foreach ($imgs as $img) { 
+		echo $img . '<br /><img src="' . $img . '" height="150"><hr />' . "\n"; 
+	}
+}
+$imgs = ob_get_clean();
+
+?>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><style>
+	* {
+	font-family: sans-serif;
+	font-size: 10pt;
+	float: left;
+	text-align:right;
+	}
+	img {
+
+		float: left;
+	}
+	hr {
+		clear: both;
+		float: none;
+	}
+</style>
+<script>
+	window.onload = function () {
+		var imgs = document.getElementsByTagName('img');
+		for (var i in imgs) {
+			console.log(imgs[i].src);
+			imgs[i].onclick = function () {
+				var img = document.createElement('img');
+				img.src = this.src.replace('/sm', '');
+				img.setAttribute('style', 'position: fixed; top: 10px; left: 10px; z-index: 100; border: solid 2px #000; ');
+				document.body.appendChild(img);
+				img.onclick = function () { img.style.display = 'none'; }
+			};
+		}
+	}
+</script>
+
+</head><body><?= $imgs ?></body></html>
