ed0fd5c27c85fea23820a56c73a22f6f32f09352
glenn <glenn@fireworkswebsites.com.au>
2014-03-22 14:58:59 +1000

    javascript version of the ADSL speed calculator

---
+++ js/ADSL speed calculator/attenuation.js
@@ -0,0 +1,16 @@
+var lookup = {};
+lookup.attenuation = {
+	100: {	dB: 1.381,	adsl1: 8000,	adsl2: 24000,	adsl1max: 8000,	adsl2max: 24000},
+	500: {	dB: 6.905,	adsl1: 8000,	adsl2: 24000,	adsl1max: 8000,	adsl2max: 24000},
+	900: {	dB: 12.429,	adsl1: 8000,	adsl2: 23000,	adsl1max: 8000,	adsl2max: 24000},
+	1300: {	dB: 17.953,	adsl1: 8000,	adsl2: 21000,	adsl1max: 8000,	adsl2max: 24000},
+	1700: {	dB: 23.477,	adsl1: 8000,	adsl2: 19000,	adsl1max: 8000,	adsl2max: 24000},
+	2100: {	dB: 29.001,	adsl1: 8000,	adsl2: 16000,	adsl1max: 8000,	adsl2max: 24000},
+	2500: {	dB: 34.525,	adsl1: 7600,	adsl2: 12000,	adsl1max: 8000,	adsl2max: 24000},
+	2900: {	dB: 40.049,	adsl1: 7000,	adsl2: 9000,	adsl1max: 8000,	adsl2max: 24000},
+	3300: {	dB: 45.573,	adsl1: 5900,	adsl2: 7000,	adsl1max: 8000,	adsl2max: 24000},
+	3700: {	dB: 51.097,	adsl1: 4600,	adsl2: 4700,	adsl1max: 8000,	adsl2max: 24000},
+	4100: {	dB: 56.621,	adsl1: 3300,	adsl2: 3600,	adsl1max: 8000,	adsl2max: 24000},
+	4500: {	dB: 62.145,	adsl1: 2100,	adsl2: 2100,	adsl1max: 8000,	adsl2max: 24000},
+	4900: {	dB: 67.669,	adsl1: 1200,	adsl2: 1600,	adsl1max: 8000,	adsl2max: 24000}
+};
