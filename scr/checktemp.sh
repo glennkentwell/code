2bce89cc83f8adfaf2931ae227e71305ae9ecf94
Glenn <git@kentwell.net>
2014-05-12 09:38:48 +1000

    remove hard-coded number of CPU cores, now discovers and iterates the cores itself. When the temp gets hot it will read out the actual temperature too, not a hard-coded value

--- scr/checktemp.sh
+++ scr/checktemp.sh
@@ -5,9 +5,13 @@
 
 # get each of the temps from 'sensors'
 # this is pretty customised for the sensors in my machine lollll
-for core in {0..3}
+
+numcores=$(grep cores /proc/cpuinfo |uniq|awk '{print $4}')
+core=0
+while [ "$core" -lt "$numcores" ]
 do
 	temps[$core]=`sensors|grep Core|awk '{print $3}'|head -n $(($core+1))|tail -n 1`	
+    core=$(($core+1))
 done
 temps[$(($core+1))]=`sensors|grep Virtual -A 2 |grep temp1|awk '{print $2}'`
 temps[$(($core+2))]=`sensors|grep Virtual -A 2 |grep temp2|awk '{print $2}'`
@@ -32,16 +36,19 @@ fi
 if [ $inttemp -gt 1000 ]
 then
 	notify-send -u critical "VERY HOT!  Do something!"
-	if [ $speak -eq 1 ]; then espeak "WARNING WARNING one hundred degrees "; touch ~/.temphistory/spoken; fi
+    espeak $(($inttemp/10))
+    #if [ $speak -eq 1 ]; then espeak "WARNING $(($inttemp/10)) degrees "; touch ~/.temphistory/spoken; fi
 elif [ $inttemp -gt 900 ]
 then
 	notify-send -u low "Hot! Temps over 90C check it out"
-	if [ $speak -eq 1 ]; then espeak "Over 90 degrees"; touch ~/.temphistory/spoken; fi
+    espeak $(($inttemp/10))
+    #if [ $speak -eq 1 ]; then espeak "$(($inttemp/10)) degrees"; touch ~/.temphistory/spoken; fi
 elif [ $inttemp -gt 800 ]
 then
 	notify-send -u low "getting warmer - over 80C"
 fi
 
+exit
 
 #save some temps for posterity, to see if we can see if the fing is overheatin when it shuts downnn
 when=`date +%Y%m%d%H%M%S`
