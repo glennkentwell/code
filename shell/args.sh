99ba3fb95dc4dd24096bb8b1176b78fac3ea2c87
Glenn <g@kentwell.net>
2015-12-13 04:19:37 +1100

    made args.sh actually a demonstration that can be used as a reminder

--- shell/args.sh
+++ shell/args.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# all command-line arguments that are passed
+echo '$* = all arguments'
+echo $*
+
+echo ' '
+echo '**************'
+echo ' '
+
+# current program?
+echo '$0 = the program itself is the 0th argument'
 echo $0
+
+echo ' '
+echo '**************'
+echo ' '
+
+# first argument
+echo '$1 and $2 etc are the arguments that are passed '
+echo ' to the program '
 echo $1
 echo $2
