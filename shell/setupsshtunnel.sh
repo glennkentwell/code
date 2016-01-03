61786ef0297009a5b87437267f8480a1d3aaf966
Glenn <g@kentwell.net>
2016-01-03 17:06:56 +1100

    remove specific host string and add stuff about ssh config file alias

--- shell/setupsshtunnel.sh
+++ shell/setupsshtunnel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source ~/.bash_functions 
 
-tunnelcmd="ssh -f -N sshtunnel"
-# ssh -R 22223:localhost:22 glenn@glenn.io
-
 # http://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/
 # ssh args: -f means go to background, -N means don't run a remote command
+# you need to have a host alias setup in ~/.ssh/config called sshtunnel
+tunnelcmd="ssh -f -N sshtunnel"
+
 numtunnels=$( ps aux | grep -v grep | grep -c "$tunnelcmd" ) 
 debugmsg "$numtunnels" tunnels 
 if [ "$numtunnels" == "0" ]
