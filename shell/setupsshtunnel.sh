a2fc0c26c403b663f04ccea8883f8b1daf26a237
Glenn Kentwell <glenn@kentwell.net>
2017-04-22 08:02:25 +1000

    dont bother if the server isn\'t able to be pinged

--- shell/setupsshtunnel.sh
+++ shell/setupsshtunnel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source ~/.bash_functions 
 
+# if the server isn't contactable then don't even bother
+ping -c 1 -q glenn.io &> /dev/null
+if [ $? -ne 0 ]; then exit; fi
+
 # http://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/
 # ssh args: -f means go to background, -N means don't run a remote command
 # you need to have a host alias setup in ~/.ssh/config called sshtunnel
